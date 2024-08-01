e3700659b48a9a85cc30bef422683c7f99b37221
ParthibanJ15 <130734175+ParthibanJ15@users.noreply.github.com>
2024-08-01 12:25:08 +0530

    Delete appointmentsystem directory

--- appointmentsystem/src/main/java/com/appointmentsystem/appointmentsystem/repository/DoctorRepo.java
+++
@@ -1,7 +0,0 @@
-package com.appointmentsystem.appointmentsystem.repository;
-
-import com.appointmentsystem.appointmentsystem.model.DoctorDet;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepo extends JpaRepository<DoctorDet,Long> {
-}
